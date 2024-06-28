1662cc7689fd5399e861c2892ca8f07691e03a50
Karan-hub299 <148038639+Karan-hub299@users.noreply.github.com>
2024-06-28 14:29:30 +0530

    Added background and text

--- src/Components/Landing/Page_3/CampusAmbassador.js
+++ src/Components/Landing/Page_3/CampusAmbassador.js
@@ -1,81 +1,56 @@
 import React from 'react';
 import './CampusAmbassador.css';
-// import tree1 from './tree 1.webp';
-// import tree2 from './tree 2.webp';
-// import mathura from './mathura@3x 1.webp';
-// import kolkata1 from './kolkata@3x 1.webp';
-// import kolkata2 from './kolkata@3x 2.webp';
-// import bird1 from './purepng.com-gull-birdbirdsflyanimals-6315229364462xrbq 1.webp';
-// import bird2 from './purepng.com-gull-birdbirdsflyanimals-6315229364462xrbq 2 (1).webp';
-// import vector19 from './Vector 19.svg';
-// import vector18 from './Vector 18.svg';
-// import vector17 from './Vector 17.svg';
-// import vector16 from './Vector 16.svg';
-// import vector15 from './Vector 15.svg';
-// import vector11 from './Vector 11.svg';
-// import frame25 from './Frame 25.svg';
-// import ellipse15 from './Ellipse 15.svg';
-// import ellipse16 from './Ellipse 16.svg';
-// import ellipse17 from './Ellipse 17.svg';
-// import ellipse128 from './Ellipse 128.webp';
-// import ellipse129 from './Ellipse 129.webp';
-// import wall1 from './wall 1@3x 2.webp';
-// import wall2 from './wall 1@3x 3.webp';
+import cloud2 from './cloud2.svg';
 
-const CampusAmbassador = () => {
+function CampusAmbassador() {
+
   return (
-    <div className="campus-ambassador-container">
+    <div className='campus-ambassador-container'>
       <div className="text-frame">
         <div className="text-item">
-          <h2>CERTIFICATES</h2>
-          <p>Certificate to add in your Resume. You can use this experience to build a portfolio.</p>
+          <h2 id='certificates'>CERTIFICATES</h2>
+          <p id='certificate-text'>
+            <ul>
+              <li>Certificate to add in your Resume. You can use this experience to build a portfolio.</li>
+            </ul>
+          </p>
         </div>
         <div className="text-item">
-          <h2>LEADERSHIP QUALITIES</h2>
-          <p>Campus Ambassador at our fest gets the chance to represent their college in our fest.</p>
+          <h2 id='leadershipQualities'>LEADERSHIP QUALITIES</h2>
+          <p id='leadershipQualities-text'>
+            <ul>
+              <li>Campus Ambassador at our fest gets the chance to represent their college in our fest.</li>
+            </ul>
+          </p>
         </div>
         <div className="text-item">
-          <h2>FREE ENTRY</h2>
-          <p>Free entry for the Campus Ambassador on 30+ participation.</p>
+          <h2 id='freeEntry'>FREE ENTRY</h2>
+          <p id='freeEntry-text'>
+            <ul>
+              <li>Free entry for the Campus Ambassador on 30+ participation.</li>
+            </ul>
+          </p>
         </div>
         <div className="text-item">
-          <h2>INTERNSHIP OPPORTUNITY</h2>
-          <p>You can use this experience to grab internship opportunities from our sponsors.</p>
+          <h2 id='internshipOpportunity'>INTERNSHIP OPPORTUNITY</h2>
+          <p id='internshipOpportunity-text'>
+            <ul>
+              <li>You can use this experience to grab internship opportunities from our sponsors.</li>
+            </ul>
+          </p>
         </div>
         <div className="text-item">
-          <h2>WORKSHOPS</h2>
-          <p>Free entry to the workshops conducted by our partners.</p>
+          <h2 id='workshops'>WORKSHOPS</h2>
+          <p id='workshops-text'>
+            <ul>
+              <li>Free entry to the workshops conducted by our partners.</li>
+            </ul>
+          </p>
         </div>
       </div>
-      <div className="images">
-        <div className="image bus" />
-        <div className="image vector18" />
-        <div className="image ellipse16" />
-        <div className="image ellipse15" />
-        <div className="image ellipse17" />
-        <div className="image vector15" />
-        <div className="image vector16" />
-        <div className="image vector17" />
-        <div className="image ellipse128" />
-        <div className="image ellipse129" />
-        <div className="image tree2" />
-        <div className="image kolkata2" />
-        <div className="image kolkata1" />
-        <div className="image tree1" />
-        <div className="image mathura" />
-        <div className="image wall1" />
-        <div className="image kolkata2b" />
-        <div className="image wall2" />
-        <div className="image bird2" />
-        <div className="image bird1" />
-        <div className="image vector15s" />
-        <div className="image vector17s" />
-        <div className="image vector16s" />
-        <div className="image vector11" />
-        <div className="image vector19" />
-      </div>
+      <img src={cloud2} alt="cloud2" id='cloud2'/>
     </div>
-  );
-};
+  )
+}
 
-export default CampusAmbassador;
+export default CampusAmbassador
