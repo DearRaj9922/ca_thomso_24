64955ad864fb88d1413bee91194429e4a553aae4
Divya Raj <draj56227@gmail.com>
2024-06-30 22:43:01 +0530

    some css correction

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import CampusAmbassador from './Components/Landing/Page_3/CampusAmbassador';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing4 from "../src/Components/Landing/Landing4/Landing4.js";
+import Landing1 from "../src/Components/Landing/Landing1/Landing1.js";
+// import Landing2 from "../src/Components/Landing/Landing2/Landing2.js";
 import React from "react";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
     
       <Router>
         <Routes>
+          <Route path ='/' element={<Landing1/>}/>
+          {/* <Route path ='/landing2' element={<Landing2/>}/> */}
           <Route path='/landing4' element={<Landing4 />} />
           <Route path='/landing3' element={<CampusAmbassador />} />
         </Routes>
