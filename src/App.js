f05d58ea174977fa424ff52fc464097909b14fe5
Baman Irani <amany29074@gmail.com>
2024-06-30 19:08:27 +0530

    landing1 Fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import CampusAmbassador from './Components/Landing/Page_3/CampusAmbassador';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing4 from "../src/Components/Landing/Landing4/Landing4.js";
 import React from "react";
+import Landing1 from "../src/Components/Landing/Landing1/Landing1.js"
+import { Main } from './Components/Landing/Landing1/Main.jsx';
+
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
     
       <Router>
         <Routes>
+
+         
+          <Route path='/' element={<Main />} />
+          <Route path='/land' element={<Landing1 />} />
           <Route path='/landing4' element={<Landing4 />} />
           <Route path='/landing3' element={<CampusAmbassador />} />
         </Routes>
