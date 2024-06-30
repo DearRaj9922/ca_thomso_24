4e21c190b380ba94ade29421a867fa56da5ed659
det-aman <youtheducated072@gmail.com>
2024-06-30 19:15:31 +0530

    Update Landing2.js

--- src/Components/Landing/Landing2/Landing2.js
+++ src/Components/Landing/Landing2/Landing2.js
@@ -4,14 +4,14 @@ import scooter from "./Assets/scooter.svg"
 export default function Landing2() {
   return (
     <div>
-      <div className={styles.first}>
-      <p className={styles.text}>What is Campus
+      <div className="first">
+      <p className="text">What is Campus
         Ambassador Program?</p>
-        <img src={cloud} alt="cloud" className={styles.cloud1}/>
-        <img src={cloud} alt="cloud" className={styles.cloud2}/>
-        <img src={cloud} alt="cloud" className={styles.cloud3}/>
-        <img src={cloud} alt="cloud" className={styles.cloud4}/>
-        <img src={scooter} alt="scooter" className={styles.scooter} />
+        <img src={cloud} alt="cloud" className="cloud1"/>
+        <img src={cloud} alt="cloud" className="cloud2"/>
+        <img src={cloud} alt="cloud" className="cloud3"/>
+        <img src={cloud} alt="cloud" className="cloud4"/>
+        <img src={scooter} alt="scooter" className="scooter" />
       </div>
 
     </div>
