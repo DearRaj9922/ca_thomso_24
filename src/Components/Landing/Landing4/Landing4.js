d44e8ee5ea46a0f2437735fc98f8016b771c78c3
Divya Raj <draj56227@gmail.com>
2024-07-01 23:01:21 +0530

    added navigation to registration button

--- src/Components/Landing/Landing4/Landing4.js
+++ src/Components/Landing/Landing4/Landing4.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Landing4.css";
+import { useNavigate, Link } from "react-router-dom";
 import landingbgdesk4 from "../../Assets/land4.webp";
 import landingbgmon4 from "../../Assets/land4-mobile.webp";
 import textimg from "../../Assets/WHAT ARE YOU WAITING FOR_.svg";
@@ -7,6 +8,7 @@ import textimgmob from "../../Assets/land4text.svg";
 import cloud4 from "../../Assets/cloud4.svg";
 
 function Landing4() {
+  const navigate = useNavigate();
   return (
     <>
     <img src={landingbgdesk4} className="bg_img_4" alt="land-bg-4"/>
@@ -18,7 +20,7 @@ function Landing4() {
       
       </div>
       <div className="reg-button-div">
-        <button className="reg-button">Register Now!</button>
+        <button onClick={() => navigate("/registration")} className="reg-button">Register Now!</button>
       </div>
       <div className="clouds">
       <img src={cloud4} className="cloud4-1" alt="cloud-4-1"/>
@@ -39,7 +41,7 @@ function Landing4() {
       <img src={textimgmob} className="head-text-mob" alt="text-i-mob"/>
       </div>
       <div className="reg-button-div">
-        <button className="reg-button">Register Now!</button>
+        <button onClick={() => navigate("/registration")} className="reg-button">Register Now!</button>
       </div>
     </div>
     </>
