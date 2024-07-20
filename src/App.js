8edd2d3015521541b5f1515a987c90e2f9ff739e
VINAYAK <vinayakjat.ps11@gmail.com>
2024-07-20 23:07:55 +0530

    Vinayak has done the changes @11:07PM

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import CampusAmbassador from "./Components/Landing/Page_3/CampusAmbassador";
 import Landing4 from "./Components/Landing/Landing4/Landing4.js";
 import Landing1 from "./Components/Landing/Landing1/Landing1.js";
 import Landing2 from "./Components/Landing/Landing2/Landing2.js";
-import PersonalDetails from './Components/registration/step1/personalDetails.jsx';
+import PersonalDetails from "./Components/registration/step1/personalDetails.jsx";
 import Landing5 from "./Components/Landing/Landing5/Landing5.js";
 import Emailverify from "./Components/registration/register/form.js";
 import Emailverified from "./Components/registration/EmailVerify/EmailVerify.js";
-import Login from './Components/registration/login/login.js';
+import Login from "./Components/registration/login/login.js";
 import HeroSection from "./Components/Landing/Hero Section/HeroSection";
 import Footer from "./Components/Footer/Footer.js";
 import CombinedLandingPage from "./Components/CombinedLandingPage/CombinedLandingPage.js";
@@ -20,16 +20,54 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<ParallaxWrapper><HeroSection className="parallax-element" data-speed="0.5" /></ParallaxWrapper>} />
-          <Route path="/landing2" element={<ParallaxWrapper><Landing2 className="parallax-element" data-speed="0.4" /></ParallaxWrapper>} />
-          <Route path='/landing3' element={<ParallaxWrapper><CampusAmbassador className="parallax-element" data-speed="0.2" /></ParallaxWrapper>} />
-          <Route path='/landing4' element={<ParallaxWrapper><Landing4 className="parallax-element" data-speed="0.3" /></ParallaxWrapper>} />
-          <Route path='/landing5' element={<ParallaxWrapper><Landing5 className="parallax-element" data-speed="0.1" /></ParallaxWrapper>} />
-          <Route path='/mainlanding' element={<CombinedLandingPage />} />
-          <Route path='/registration' element={<PersonalDetails />} />
-          <Route path='/verify' element={<Emailverify />} />
-          <Route path='/emailverified' element={<Emailverified />} />
-          <Route path='/login' element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ParallaxWrapper>
+                <HeroSection className="parallax-element" data-speed="0.5" />
+              </ParallaxWrapper>
+            }
+          />
+          <Route
+            path="/landing2"
+            element={
+              <ParallaxWrapper>
+                <Landing2 className="parallax-element" data-speed="0.4" />
+              </ParallaxWrapper>
+            }
+          />
+          <Route
+            path="/landing3"
+            element={
+              <ParallaxWrapper>
+                <CampusAmbassador
+                  className="parallax-element"
+                  data-speed="0.2"
+                />
+              </ParallaxWrapper>
+            }
+          />
+          <Route
+            path="/landing4"
+            element={
+              <ParallaxWrapper>
+                <Landing4 className="parallax-element" data-speed="0.3" />
+              </ParallaxWrapper>
+            }
+          />
+          <Route
+            path="/landing5"
+            element={
+              <ParallaxWrapper>
+                <Landing5 className="parallax-element" data-speed="0.1" />
+              </ParallaxWrapper>
+            }
+          />
+          <Route path="/mainlanding" element={<CombinedLandingPage />} />
+          <Route path="/registration" element={<PersonalDetails />} />
+          <Route path="/verify" element={<Emailverify />} />
+          <Route path="/emailverified" element={<Emailverified />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
