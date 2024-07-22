e84cb43244eaf8d0bc65ed93b53674847cef15fc
Dewansh Upadhyay <89184066+dew-op@users.noreply.github.com>
2024-07-23 03:04:53 +0530

    Add ProfileCard

--- src/Components/ProfileCard/ProfileCard.js
+++ src/Components/ProfileCard/ProfileCard.js
@@ -1,37 +1,48 @@
 import React from 'react';
-import './ProfileCard.css'
+import './ProfileCard.css';
+import Copy from '../Assets/copy.svg';
+import User from '../Assets/guyprofile.svg';
 
 function ProfileCard(props) {
     return (
         <div className="profilecard-container">
-            <div className='profilecard-referral'>Referral Code</div>
-            <div>Dhruv Bhatia</div>
-            <div>
-                <div>
-                    <div>Education Details</div>
-                    <div>
-                        <div className="items">
+            <div className="profilecard-userimg">
+                <img src={User} alt="profile" />
+            </div>
+            <div className='profilecard-referral'>
+                <p className="profilecard-referral-text">Referral Code</p>
+                <div className="profilecard-referral-code">
+                    <p>165GZXV4</p>
+                    <img src={Copy} alt="copy" />
+                </div>
+            </div>
+            <div className='profilecard-name'>Dhruv Bhatia</div>
+            <div className='profilecard-details'>
+                <div className='profilecard-details-div'>
+                    <div className='profilecard-details-head'>Education Details</div>
+                    <div className='profilecard-details-text'>
+                        <div className="items-head">
                             <div>College</div>
-                            <div>Indian Institute of Technology Roorkee</div>
+                            <div>Degree and Year</div>
                         </div>
                         <div className="items">
-                            <div>Degree and Year</div>
+                            <div>Indian Institute of Technology Roorkee</div>
                             <div>B.Arch 3rd</div>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div>Contact Info.</div>
-                    <div>
-                        <div className="items">
+                <div className='profilecard-details-div'>
+                    <div className='profilecard-details-head'>Contact Info.</div>
+                    <div className='profilecard-details-text'>
+                        <div className="items-head">
                             <div>Email Id</div>
-                            <div>abc@gmail.com</div>
+                            <div>Phone no.</div>
                         </div>
                         <div className="items">
-                            <div>Phone no.</div>
+                            <div>abc@gmail.com</div>
                             <div className="inputbox">
                                 <div><input/></div>
-                                <div>Save</div>
+                                <button>Save</button>
                             </div>
                         </div>
                     </div>
