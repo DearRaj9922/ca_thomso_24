430d18321ca666fd333af35bd2e5ef57025a0447
Deepak <deepak988088@gmail.com>
2024-07-24 22:32:00 +0530

    fixed the images

--- src/Components/Landing/Hero Section/HeroSection.jsx
+++ src/Components/Landing/Hero Section/HeroSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./hero.css";
 import clouds from "../../Assets/clouds-hero.svg";
 import landing from "../../Assets/Landing page image.svg";
+import Footer from "../../Footer/Footer";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 
@@ -17,10 +18,10 @@ const HeroSection = () => {
 
         <div className="hero-reg-btn">
           <Link
-          to="/registration">
-              <button className="hero-reg">Register Now</button>
+            to="/registration">
+            <button className="hero-reg">Register Now</button>
           </Link>
-          
+
         </div>
 
         <div className="hero_image">
@@ -28,6 +29,7 @@ const HeroSection = () => {
           <img src={landing} alt="" className="landingImage" />
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
