4e684ab24fdc5247bfe6f7ae7a07a2d53f2cd40c
Dewansh Upadhyay <89184066+dew-op@users.noreply.github.com>
2024-07-25 02:18:55 +0530

    AOS

--- src/Components/Landing/Hero Section/HeroSection.jsx
+++ src/Components/Landing/Hero Section/HeroSection.jsx
@@ -1,32 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./hero.css";
 import clouds from "../../Assets/clouds-hero.svg";
 import landing from "../../Assets/Landing page image.svg";
 import Footer from "../../Footer/Footer";
 import { Link } from "react-router-dom";
-import Navbar from "./Navbar";
+import AOS from "aos";
+import 'aos/dist/aos.css';
 
 const HeroSection = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true });
+
+    AOS.refresh();
+  }, []);
+
   return (
     <div>
       {/* <Navbar/> */}
-      <div className="hero_wrapper ">
+      <div className="hero_wrapper" data-aos="fade-up">
         <div className="hero_content">
           <p>CAMPUS AMBASSADOR</p>
           <p className="program-text">PROGRAM</p>
         </div>
 
         <div className="hero-reg-btn">
-          <Link
-          to="/registration">
-              <button className="hero-reg ml-[50px]">Register Now</button>
+          <Link to="/registration">
+            <button className="hero-reg ml-[50px]">Register Now</button>
           </Link>
 
         </div>
 
         <div className="hero_image">
-          <img className="clouds-landing" src={clouds} alt="" />
-          <img src={landing} alt="" className="landingImage" />
+          <img className="clouds-landing" src={clouds} alt="Clouds" />
+          <img src={landing} alt="Landing" className="landingImage" />
         </div>
       </div>
       <Footer />
--- src/Components/Landing/Landing2/Landing2.js
+++ src/Components/Landing/Landing2/Landing2.js
@@ -1,47 +1,52 @@
-import React from 'react'
-import "./Landing2.css";
-import back2 from "../../Assets/land2.svg";
-import back2mob from "../../Assets/land2-mobile.webp";
-import land2text from "../../Assets/land2textmob.svg";
-import scooter from "../../Assets/scooter.svg";
+import React, { useEffect } from 'react';
+import './Landing2.css';
+import back2 from '../../Assets/land2.svg';
+import back2mob from '../../Assets/land2-mobile.webp';
+import land2text from '../../Assets/land2textmob.svg';
+import scooter from '../../Assets/scooter.svg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Landing2() {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true });
+
+    AOS.refresh();
+  }, []);
+
   return (
-    <div className='relative Landing2wrapper'>
-      <img src={back2} className=' bg_img_2' alt="bg-2-img" />
-      <img src={back2mob} className=' bg_img_2_mob' alt="bg" />
+    <div className="relative Landing2wrapper" data-aos="fade-left">
+      <img src={back2} className="bg_img_2" alt="bg-2-img" />
+      <img src={back2mob} className="bg_img_2_mob" alt="bg" />
       <div className="landing2">
         <div className="landing2-text-box">
           <div className="landing2-text">
-            <h1>
-              What is Campus Ambassdor Program?
-            </h1>
+            <h1>What is Campus Ambassador Program?</h1>
           </div>
-          <div className="landing2-details">
+          <div className="landing2-details" data-aos="fade-left" data-aos-delay="200">
             <h1>
               As part of the student campus ambassador program, THOMSO offers students the chance to represent and promote the organization at their colleges and universities. Their responsibilities comprise developing our presence and promoting THOMSO events among students and educators by serving as a link between their colleges and us.
             </h1>
           </div>
         </div>
-        <div className="scooter-div">
+        <div className="scooter-div" data-aos="fade-left" data-aos-delay="400">
           <div className="scooter-img-div">
-            <img src={scooter} className=' scooter-img' alt="scooter-img" />
+            <img src={scooter} className="scooter-img" alt="scooter-img" />
           </div>
         </div>
       </div>
       <div className="landing2-mobile">
-        <div className="landing2-text-box">
+        <div className="landing2-text-box" data-aos="fade-left">
           <div className="landing2-text">
-            <img src={land2text} className=' land2text-img' alt="land2-text" />
+            <img src={land2text} className="land2text-img" alt="land2-text" />
           </div>
         </div>
-        <div className="scooter-div">
+        <div className="scooter-div" data-aos="fade-left" data-aos-delay="200">
           <div className="scooter-img-div">
-            <img src={scooter} className='scooter-img' alt="scooter-img" />
+            <img src={scooter} className="scooter-img" alt="scooter-img" />
           </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
