c0740c6d92be34d360be28cf7fca4fd83b2b7928
anamikapatel0120 <anamikap993@gmail.com>
2024-07-24 23:39:10 +0530

    added scrollbar

--- src/Components/ProfileMilestone/ProfileMilestone.js
+++ src/Components/ProfileMilestone/ProfileMilestone.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import './ProfileMilestone.css'
-
+import './ProfileMilestone.css';
 
 const ReferralData = [
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-    {score:20, name: "Mukul Dhiman"},
-]
-function ProfileMilestone(props) {
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+    { score: 20, name: "Mukul Dhiman" },
+];
+
+function ProfileMilestone() {
     return (
         <div className="profile-milestone">
-            {ReferralData.map((item, index)=>{
-                return(<><div className="milestone-item">
-                <div className="milestone-score">+{item.score}</div>
-                <div className="milestone-inner-text">
-                    <div>Referral Accepted</div>
-                    <div className="milestone-secondary">{item.name} joined</div>
-                </div>
-
-            </div>
-            <div><hr/></div></>)
-            })}
-
+            {ReferralData.map((item, index) => (
+                <React.Fragment key={index}>
+                    <div className="milestone-item">
+                        <div className="milestone-score">+{item.score}</div>
+                        <div className="milestone-inner-text">
+                            <div>Referral Accepted</div>
+                            <div className="milestone-secondary">{item.name} joined</div>
+                        </div>
+                    </div>
+                    <div><hr /></div>
+                </React.Fragment>
+            ))}
         </div>
     );
 }
