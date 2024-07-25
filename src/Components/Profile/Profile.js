9eb71c10d658d25947efd669ef649336d7fbff10
Keshav45067 <keshav_g1@ce.iitr.ac.in>
2024-07-26 02:09:32 +0530

    d

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -6,20 +6,75 @@ import ProfileCard from "../ProfileCard/ProfileCard";
 import banner from '../Assets/bannermain.svg'
 import ProfileMilestone from "../ProfileMilestone/ProfileMilestone";
 import {useNavigate} from "react-router-dom";
+import { fetchUser, logout } from "../User/UserActions";
 import axios from "axios";
-
+import { Store } from "../../Config/Store";
+import { connect } from "react-redux";
 
 function Profile(props) {
-const navigate = useNavigate();
+    const { dispatch } = Store;
+    const [display, setDisplay] = useState(true);
+    const [logoutModal, setLogoutModal] = useState(false);
+    // const [editModal, setEditModal] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [openClg, setOpenClg] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+    const [inputValue, setInputValue] = useState();
+    const { userDetails: user, loading } = props;
+    // const [user, setuser] = useState({});
+    const [error, setError] = useState(false);
+    const [Loading, setLoading] = useState(false);
+    const [errorMsg, setErrorMsg] = useState("");
+
+
+    const navigate = useNavigate();
+
 const [User, setUser] = useState(null);
+const onSubmit = async (e) => {
+        e.preventDefault();
+
+        setLoading(true);
+        const { dispatch } = Store;
+        try {
+            const response = await axios.put(
+                `/apiV1/registerca/${localStorage.getItem("user_id")}`,
+                User
+            );
+            const { data } = response;
+            if (response.status === 200) {
+                setLoading(false);
+                setOpenModal(!openModal);
+                dispatch(fetchUser());
+            }
+            // window.location.reload();
+            setLoading(false);
+            setOpenModal(!openModal);
+        } catch (err) {
+            setLoading(false);
+            const { data } = err?.response;
+            console.log("register Error:", data);
+            var errorData = "";
+            setErrorMsg(errorData);
+            setError(true);
+            setLoading(false);
+            setOpenModal(!openModal);
+        }
+    };
+
 const getUser = async()=>{
     const res = await axios.get(`http://35.154.76.67/apiV1/registerca/${localStorage.getItem("user_id")}`);
     console.log(res.data)
+    dispatch(fetchUser());
     setUser(res.data);
     // setUser(res.data);
 }
+    const tmkc = async()=>{
+    const res = await axios.get("http://127.0.0.1:8000/apiV1/current_user_ca");
+    console.log(res)
+    }
     useEffect(() => {
         getUser()
+        tmkc();
     }, []);
     return (
         <div className="profile-main-container">
@@ -49,14 +104,14 @@ const getUser = async()=>{
                                 <div>Ranking</div>
                             </div>
                             <div className="banner-section">
-                                <div>312</div>
+                                <div>{User? User?.total_score : "-"}</div>
                                 <div>
                                     <hr />
                                 </div>
                                 <div>Score</div>
                             </div>
                             <div className="banner-section">
-                                <div>15</div>
+                                <div>{User? User?.referral_score:"-"}</div>
                                 <div>
                                     <hr />
                                 </div>
@@ -76,4 +131,21 @@ const getUser = async()=>{
     );
 }
 
-export default Profile;
+const mapStateToProps = (state) => {
+    let userDetails = state.user.user;
+    let loading = state.user.loading;
+
+    return {
+        userDetails,
+        loading,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchUsers: (params) => dispatch(fetchUser(params)),
+        logouts: (params) => dispatch(logout(params)),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
+import axios from "axios";
 import store from './store.js';
 import reportWebVitals from './reportWebVitals';
-
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
+// axios.defaults.baseURL = "https://api1.thomso.in";
+axios.defaults.headers.common.Authorization = `${
+    localStorage.getItem("token") === null
+        ? ``
+        : `Bearer ${localStorage.getItem("token")}`
+}`;
+axios.defaults.headers.post["Content-Type"] = "application/json";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <App />
+        <App history={history}/>
     </Provider>
  
 );
