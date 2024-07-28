125a5a47bc70d3663dbb2b3c0a6cd4c9ecefc28f
Deepak <deepak988088@gmail.com>
2024-07-28 23:32:52 +0530

    fix errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CampusAmbassador from "./Components/Landing/Landing3/CampusAmbassador";
 import Landing4 from "./Components/Landing/Landing4/Landing4.js";
 // import Landing1 from "./Components/Landing/Landing1/Landing1.js";
@@ -20,34 +20,14 @@ import Referrals from "./Components/Referrals/referrals.js";
 
 import CombinedLandingPage from "./Components/CombinedLandingPage/CombinedLandingPage.js";
 import Quizardry from "./Components/Quizardry/Quizardry";
+import Leaderboard from "./Components/registration/Leaderboard/Leaderboard.js";
+import LeaderRightbar from "./Components/registration/Leaderboard/LeaderRightbar.js";
 
 
 function App() {
     return (
-        <div className="App">
-            <Router>
-                <Routes>
-                    <Route path="/landing1" element={<HeroSection />} />
-                    <Route path="/landing2" element={<Landing2 />} />
-                    <Route path='/landing3' element={<CampusAmbassador />} />
-                    <Route path='/landing4' element={<Landing4 />} />
-                    <Route path='/landing5' element={<Landing5 />} />
-                    <Route path='/' element={<CombinedLandingPage />} />
-                    <Route path='/registration' element={<PersonalDetails />} />
-                    <Route path='/verify' element={<Emailverify />} />
-
-                    <Route path='/emailverified' element={<Emailverified/>}/>
-                    <Route path='/login' element={<Login/>}/>
-                    <Route path='/profile' element={<Profile/>}/>
-                    <Route path='/profiledetail' element={<ProfileDetails/>}/>
-                    <Route path='/referrals' element={<Referrals/>}/>
-
-                        {/*<Route path='/quizardry' element={<Quizardry/>}/>*/}
-
-                        </Routes>
-                        </Router>
-                        </div>
-                        );
-                    }
+        <Leaderboard />
+    );
+}
 
 export default App;
